12dbbec76931ff4215c9fd415e334ce0b07f6923
icee11 <mars_011@yahoo.com>
2019-03-02 00:37:00 +0800

    Observer Pattern exercise

---
+++ src/GrabStocks.java
@@ -0,0 +1,24 @@
+
+public class GrabStocks {
+	
+	public static void main(String[] args) {
+		StockGrabber stockGrabber = new StockGrabber();
+		StockObserver observer1 = new StockObserver(stockGrabber);
+		
+		stockGrabber.setIBMPrice(197.00);
+		stockGrabber.setAaplPrice(677.60);
+		stockGrabber.setGoogPrice(676.40);
+		
+		StockObserver observer2 = new StockObserver(stockGrabber);
+		
+		stockGrabber.setIBMPrice(197.00);
+		stockGrabber.setAaplPrice(677.60);
+		stockGrabber.setGoogPrice(676.40);
+		
+		stockGrabber.unregister(observer1);
+		
+		stockGrabber.setIBMPrice(197.00);
+		stockGrabber.setAaplPrice(677.60);
+		stockGrabber.setGoogPrice(676.40);
+	}
+}
---
+++ src/StockGrabber.java
@@ -0,0 +1,65 @@
+import java.util.ArrayList;
+
+public class StockGrabber implements Subject{
+
+	private ArrayList<Observer> observers;
+	private double ibmPrice;
+	private double aaplPrice;
+	private double googPrice;
+	
+	public StockGrabber() {
+		observers = new ArrayList();
+	}
+	
+	@Override
+	public void register(Observer observer) {
+		observers.add(observer);
+		
+	}
+
+	@Override
+	public void unregister(Observer observer) {
+		int observerIndex = observers.indexOf(observer);
+		observers.remove(observerIndex);
+		
+		System.out.println("Observer " + (observerIndex + 1) + " deleted");
+		
+		
+	}
+
+	@Override
+	public void notifyObserver() {
+		for (Observer observer : observers) {
+			observer.update(ibmPrice, aaplPrice, googPrice);
+		}
+		
+	}
+	
+	public void setIBMPrice(double newIBMPrice) {
+		this.ibmPrice = newIBMPrice;
+		notifyObserver();
+	}
+	
+	public void setGoogPrice(double newGoogPrice) {
+		this.googPrice = newGoogPrice;
+		notifyObserver();
+	}
+	
+	public void setAaplPrice(double newAaplPrice) {
+		this.aaplPrice = newAaplPrice;
+		notifyObserver();
+	}
+	
+	public double getIBMPrice(){
+		return this.ibmPrice;
+	}
+	
+	public double getGoogPrice(){
+		return this.googPrice;
+	}
+	
+	public double getAaplPrice(){
+		return this.aaplPrice;
+	}
+
+}
