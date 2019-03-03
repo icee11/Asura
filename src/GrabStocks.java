2de7d0802c1430014349b4ffa9deb21ec08d9bdb
icee11 <mars_011@yahoo.com>
2019-03-03 13:47:46 +0800

    Add Thread

--- src/GrabStocks.java
+++ src/GrabStocks.java
@@ -21,6 +21,16 @@ public class GrabStocks {
 		stockGrabber.setAaplPrice(677.60);
 		stockGrabber.setGoogPrice(676.40);
 		
-		stockGrabber.unregister(observer2);
+		//stockGrabber.unregister(observer2);
+		
+		System.out.println("Thead starts here...");
+		
+		Runnable getIBM = new GetTheStock(stockGrabber, 2, "IBM", 197.00);
+		Runnable getAAPL = new GetTheStock(stockGrabber, 2, "AAPL", 677.60);
+		Runnable getGOOG = new GetTheStock(stockGrabber, 2, "GOOG", 676.40);
+		
+		new Thread(getIBM).start();
+		new Thread(getAAPL).start();
+		new Thread(getGOOG).start();
 	}
 }
